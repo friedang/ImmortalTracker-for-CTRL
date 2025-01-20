eef6ce260a31e6774b3abc7f3dcdc29e22bf48c2
Friedrich Dang <friedrich.dang@setlabs.de>
2025-01-20 14:05:35 +0000

    Update run_all

--- run_all.sh
+++ run_all.sh
@@ -1,17 +1,17 @@
 train=True
-file_path="/workspace/immo/work_dirs/10_nusc_centerpoint_voxelnet_0075voxel_fix_bn_z/org/pseudo_infos_train_10sweeps_withvelo_filter_True.json"
+file_path="/workspace/immo/work_dirs/10_nusc_centerpoint_voxelnet_0075voxel_fix_bn_z/org/infos_val_10sweeps_withvelo_filter_True.json"
 detection_name="cp_5_seed"
 
 obj_types='car,bus,trailer,truck,pedestrian,bicycle,motorcycle,construction_vehicle,barrier,traffic_cone'
 
-python preparedata/nuscenes/detection.py --file_path $file_path --det_name $detection_name --train $train && \
-python main_nuscenes.py --name $detection_name --det_name $detection_name --config_path configs/nu_configs/cp_plus.yaml --process 150 --train $train && \
-python ./evaluation/nuscenes/result_creation.py --name $detection_name --obj_types $obj_types --train $train && \
-python ./evaluation/nuscenes/type_merge.py --name $detection_name --obj_types $obj_types --train $train
+# python preparedata/nuscenes/detection.py --file_path $file_path --det_name $detection_name --train $train && \
+# python main_nuscenes.py --name $detection_name --det_name $detection_name --config_path configs/nu_configs/cp_plus.yaml --process 150 --train $train && \
+# python ./evaluation/nuscenes/result_creation.py --name $detection_name --obj_types $obj_types --train $train && \
+# python ./evaluation/nuscenes/type_merge.py --name $detection_name --obj_types $obj_types --train $train
 
 
 ## Validation
-# python preparedata/nuscenes/detection.py --file_path $file_path --det_name $detection_name && \
-# python main_nuscenes.py --name $detection_name --det_name $detection_name --config_path configs/nu_configs/cp_plus.yaml --process 150 && \
-# python ./evaluation/nuscenes/result_creation.py --name $detection_name --obj_types $obj_types && \
-# python ./evaluation/nuscenes/type_merge.py --name $detection_name --obj_types $obj_types
+python preparedata/nuscenes/detection.py --file_path $file_path --det_name $detection_name && \
+python main_nuscenes.py --name $detection_name --det_name $detection_name --config_path configs/nu_configs/cp_plus.yaml --process 150 && \
+python ./evaluation/nuscenes/result_creation.py --name $detection_name --obj_types $obj_types && \
+python ./evaluation/nuscenes/type_merge.py --name $detection_name --obj_types $obj_types
